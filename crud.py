6deef2f8de16c64279d442329249cd20836c2507
Jirawat Yuktawathin <yuk.jirawat@gmail.com>
2019-01-10 21:54:01 +0700

    Can calculate GPA for each term

--- crud.py
+++ crud.py
@@ -1,4 +1,5 @@
 import csv
+import math
 
 
 def read_csvfile(filename):
@@ -20,18 +21,50 @@ def write_csvfile(filename, mode, data):
 
 
 def string_cleanup(string):
-    ''' Remove circle bracket, comma and space from string. '''
+    ''' Remove circle bracket, quote and space from string. '''
     string = string.strip('()').replace("'", '').replace(' ', '')
     return string
 
 
 while True:
     command = input('>')
-    if command.startswith('INSERT INTO'):
+    if command.startswith('GPA'):
+        # GPA Grade.csv 1 1
+        command = command.split()
+        table = command[1]          # Grade.csv
+        year = command[2]           # 1
+        semester = command[3]       # 1
+
+        data = read_csvfile(table)
+        select_data = [row for row in data if row[0] == year and row[1] == semester]
+        credit = [int(row[-2]) for row in select_data]
+        grade = []
+        for row in select_data:
+            if row[-1] == 'A':
+                grade.append(4)
+            elif row[-1] == 'B+':
+                grade.append(3.5)
+            elif row[-1] == 'B':
+                grade.append(3)
+            elif row[-1] == 'C+':
+                grade.append(2.5)
+            elif row[-1] == 'C':
+                grade.append(2)
+            elif row[-1] == 'D+':
+                grade.append(1.5)
+            elif row[-1] == 'D':
+                grade.append(1)
+            else:
+                grade.append(0)
+        point = [a * b for (a, b) in zip(grade, credit)]    # credit * grade
+
+        gpa = math.floor(sum(point) / sum(credit) * 100) / 100
+        print(gpa)
+    elif command.startswith('INSERT INTO'):
         # INSERT INTO Grade.csv VALUES ('1', '1', 'Data', '3', 'B+')
         command = command.split(' VALUES ')
-        table = command[0].split(' ')[2]                        # Grade.csv
-        value = [string_cleanup(command[1]).split(',')]         # [['1', '1', 'Data', '3', 'B+']]
+        table = command[0].split()[2]                       # Grade.csv
+        value = [string_cleanup(command[1]).split(',')]     # [['1', '1', 'Data', '3', 'B+']]
 
         write_csvfile(table, 'a', value)
     elif command.startswith('UPDATE'):
